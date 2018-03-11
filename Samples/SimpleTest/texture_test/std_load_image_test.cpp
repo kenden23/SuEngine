d608885c9d4c5ea5a75d0d455d9296f289617610
billsu <35948036@qq.com>
2018-03-11 17:48:52 +0800

    update image loading functions

--- Samples/SimpleTest/texture_test/std_load_image_test.cpp
+++ Samples/SimpleTest/texture_test/std_load_image_test.cpp
@@ -17,6 +17,9 @@
 #include "render/GLProgram.h"
 #include "sugl/Texture.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../../SuEngine/stb/stb_image.h"
+
 void GenerateVBO(GLuint &vbo)
 {
 	if (glIsBuffer(vbo))
@@ -33,14 +36,23 @@ void ScrollCallBackFunc(GLFWwindow *p, double x, double y)
 
 }
 
+void ResizeCB(GLFWwindow *p, int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
 int main()
 {
+	glfwInit();
+
 	int winWidth = 1024, winHeight = 768;
 	char title[] = "deliberate_practice_by_doing";
 	GLFWwindow *pwin = glfwCreateWindow(winWidth, winHeight, title, nullptr, nullptr);
+	glfwMakeContextCurrent(pwin);
 
 	glfwSetInputMode(pwin, GLFW_STICKY_KEYS, GL_TRUE);
 	glfwSetScrollCallback(pwin, ScrollCallBackFunc);
+	glfwSetFramebufferSizeCallback(pwin, ResizeCB);
 
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK)
@@ -58,7 +70,70 @@ int main()
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
+	float vertex_data[] = {
+		-0.5f, -0.5f, 0.f, 0.f, 1.f,
+		0.5f, -0.5f, 0.f, 1.f, 1.f,
+		0.5f, 0.5f, 0.f, 1.f, 0.f,
+		-0.5f, 0.5f, 0.f, 0.f, 0.f,
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
 
+	GLuint vio;
+	glGenBuffers(1, &vio);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vio);
+	unsigned int indices_data[] = {
+		0, 1, 3, 2
+	};
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_data), indices_data, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+		sizeof(float) * 5, (void *)(sizeof(float) * 3));
+
+	GLProgram glpro;
+	glpro.Init();
+	glpro.AddShader(GL_VERTEX_SHADER, "shaders/stb_load_image.vs");
+	glpro.AddShader(GL_FRAGMENT_SHADER, "shaders/stb_load_image.fs");
+	glpro.Finalize();
+	glpro.Enable();
+
+	GLuint tex_buf;
+	glGenTextures(1, &tex_buf);
+	glBindBuffer(GL_TEXTURE_2D, tex_buf);
+
+	Texture2D myTex("res/im.bmp");
+	myTex.use();
+
+	//int width, height, nrChannels;
+	//unsigned char *data = stbi_load("D:/Data ×ÊÁÏ/pretty/1.jpg", &width, &height, &nrChannels, 0);
+	//unsigned char *data = stbi_load("res/im.bmp", &width, &height, &nrChannels, 4);
+	//Su::SimpleImageInfo sif;
+	//Su::file::readImage_BMP(sif, "res/im.bmp");
+	//width = sif.width;
+	//height = sif.height;
+	//void *data = sif.data;
+
+
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, /GL_UNSIGNED_BYTE,/ data);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glClearColor(0.f, 0.5f, 0.5f, 0.f);
+	do 
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, 0);
+
+		glfwPollEvents();
+		glfwSwapBuffers(pwin);
+	} while (!glfwWindowShouldClose(pwin)
+		&& GLFW_PRESS != glfwGetKey(pwin, GLFW_KEY_ESCAPE));
 
 	return 0;
 }
